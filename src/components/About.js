2406e4cb076db8cf8621ca75a5da337e4c664f92
Nitro <cnitinrohit@gmail.com>
2023-08-23 16:57:38 +0530

    added the typewriter feature

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ const About = () => {
                     <h2>About</h2>
                     <span className='line'></span>
                     <p>I’m a passionate technology enthusiast and coding aficionado! With a dynamic personality driven by a relentless curiosity for all things, I like to explore things which could be learned and exploited. With a thirst for knowledge and a penchant for continuous learning, I embrace every challenge as an opportunity to grow. Whether it's unraveling the mysteries of complex algorithms or crafting elegant solutions to real-world problems. I take mathematics as my first step in all the problems I solve, giving me a procedural way to achieve my desired goal.</p>
-                    <p>I am Nitin Rohit, a final-year student from the Department of Electrical Engineering at the Indian Institute of Technology.</p>
+                    <p>I am Nitin Rohit, a final-year student from the Department of Electrical Engineering at the Indian Institute of Technology Kharagpur.</p>
                     {/* <button className='button'>Explore More</button> */}
                 </div>
             </div>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,14 @@
+import Typed from 'react-typed'
 import './Hero.css'
 
 const Hero = () => {
+
     return (
         <div className='hero'>
             <div className='content'>
                 <p>Call me</p>
                 <p>C Nitin Rohit</p>
-                <p>Creative Thinker</p>
+                <p> <Typed strings={["Creative Thinker","Problem Solver"]} typeSpeed={120} backSpeed={80} loop /> </p>
                 <p>Learn -&gt; Think -&gt; Execute </p>
                 {/* <button href='/' className='button'>Free Consultation</button> */}
             </div>

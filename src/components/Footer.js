af20a3fcc74a1bf75b65009c574ffb9218f2c518
Nitro <cnitinrohit@gmail.com>
2023-08-27 17:09:40 +0530

    added the purple shad

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,6 +7,9 @@ const Footer = ({state}) => {
         backgroundColor:"#3B3B3B",
         color:"white"
     }
+    const purple={
+        color:"#A270FF"
+    }
 
     return (
         <div className='footer' style={state?darkmode:{}}>
@@ -14,9 +17,9 @@ const Footer = ({state}) => {
                     <span className='line'></span>
                     <p>"The game is won in the Mind"</p>
                     <div>
-                        <a href='https://github.com/sdrawkcaBdeaR'><FaGithubAlt className='handle' style={state?darkmode:{}}/></a>
-                        <a href='https://www.linkedin.com/in/nitin-rohit-a01346114/'><FaLinkedin className='handle' style={state?darkmode:{}}/></a>
-                        <a href='https://www.instagram.com/dn_passaw/'><FaInstagram className='handle' style={state?darkmode:{}}/></a>
+                        <a href='https://github.com/sdrawkcaBdeaR'><FaGithubAlt className='handle' style={state?purple:{}}/></a>
+                        <a href='https://www.linkedin.com/in/nitin-rohit-a01346114/'><FaLinkedin className='handle' style={state?purple:{}}/></a>
+                        <a href='https://www.instagram.com/dn_passaw/'><FaInstagram className='handle' style={state?purple:{}}/></a>
                     </div>
                 </div>
         </div>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,13 +8,16 @@ const Hero = ({state}) => {
         backgroundColor:"#2B2B2B",
         color:"white"
     }
+    const purple={
+        color:"#A259FF"
+    }
 
     return (
         <div className='hero' style={state?darkmode:{}}>
             <div className='content'>
                 <p>Call me</p>
                 <p>C Nitin Rohit</p>
-                <p> <Typed strings={tags} typeSpeed={120} backSpeed={80} loop /> </p>
+                <p style={state?purple:{}}> <Typed strings={tags} typeSpeed={120} backSpeed={80} loop /> </p>
                 <p>Learn -&gt; Think -&gt; Execute </p>
                 {/* <button href='/' className='button'>Free Consultation</button> */}
             </div>

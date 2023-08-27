a1894fa7f87811e53123e90cf4a699907db18d10
Nitro <cnitinrohit@gmail.com>
2023-08-27 19:12:30 +0530

    added some changes

--- src/components/Domain.js
+++ src/components/Domain.js
@@ -26,19 +26,19 @@ const Domain = ({state}) => {
                         <img src={sde} alt='user1'/>
                         <p><span>Software development</span></p>
                         <p>The realm of software development captivates me due to its creative problem-solving and tangible impact. Embracing continuous learning in this dynamic field not only fuels my drive to innovate but also empowers me to transform abstract concepts into practical, user-centered solutions. With each line of code, I find the means to shape the digital landscape and contribute to a technology-driven future.</p>
-                        <p>"Where 0s and 1s write the language of technology."</p>
+                        <p className='quote'>"Where 0s and 1s write the language of technology."</p>
                     </div>
                     <div className='card' style={state?darkmodecard:{}}>
                         <img src={ai} alt='user1'/>
                         <p><span>Artificial Intelligence</span></p>
                         <p>The world of AI fascinates me with its boundless potential and transformative impact. Embracing the constant evolution in this dynamic field not only fuels my passion for innovation but also empowers me to craft intelligent systems that simulate human intelligence and decision-making. With each algorithm and model, I find the means to shape the future by harnessing the capabilities of AI to tackle intricate challenges and unlock new realms of possibility.</p>
-                        <p>"Turning data into insights, and innovation into possibility"</p>
+                        <p className='quote'>"Turning data into insights, and innovation into possibility"</p>
                     </div>
                     <div className='card' style={state?darkmodecard:{}}>
                         <img src={cs} alt='user1'/>
                         <p><span>Control Systems</span></p>
                         <p>Immersed in control systems, I'm drawn to their blend of theory and real-world impact. Embracing perpetual advancement fuels my passion for precision and empowers me to engineer systems that govern processes with accuracy. Each refined algorithm and feedback loop shapes outcomes, ensuring stability in intricate systems driving technology and industries.</p>
-                        <p>"Orchestrating precision in complexity"</p>
+                        <p className='quote'>"Orchestrating precision in complexity"</p>
                     </div>
                 </div>
             </div>

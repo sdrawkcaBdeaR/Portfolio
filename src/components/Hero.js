a71df0f7bac374d837c325ad70dcd5a7d5aced09
Nitro <cnitinrohit@gmail.com>
2023-09-23 01:32:25 +0530

    made elements in the hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -15,11 +15,20 @@ const Hero = ({state}) => {
     return (
         <div className='hero' style={state?darkmode:{}}>
             <div className='content'>
-                <p>Call me</p>
-                <p>C Nitin Rohit</p>
-                <p style={state?purple:{}}> <Typed strings={tags} typeSpeed={120} backSpeed={80} loop /> </p>
-                <p>Learn -&gt; Think -&gt; Execute </p>
-                {/* <button href='/' className='button'>Free Consultation</button> */}
+                <div className='split'>
+                    <div>
+                        <p>Call me</p>
+                        <p>C Nitin Rohit</p>
+                        <p style={state?purple:{}}> <Typed strings={tags} typeSpeed={120} backSpeed={80} loop /> </p>
+                        <p>Learn -&gt; Think -&gt; Execute </p>
+                    </div>
+                    <div className='box'>
+                        <div className='box1 boxx'></div>
+                        <div className='box2 boxx'></div>
+                        <div className='box3 boxx'></div>
+                        <div className='box4 boxx'></div>
+                    </div>
+                </div>
             </div>
         </div>
     )
